1f654b22309c5af8c1ec320adcfcda1f071c37dc
Romain Picard <romain.picard@oakbits.com>
2015-09-22 22:29:22 +0200

    Updated version number

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ install_requires = [
 
 setup(
     name="flock",
-    version='0.1.0',
+    version='0.2.0',
     url='https://github.com/',
     license='MIT',
     description="Smart home library for python",
